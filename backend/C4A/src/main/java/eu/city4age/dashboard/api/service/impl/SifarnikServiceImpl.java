ea463a451de60af985d15d9a077020170b9fbf52
mholclajtner <milos.holclajtner@gmail.com>
2016-12-13 15:20:26 +0100

    Web services for list of codes for ValidityDataStatus and OrderBy

--- backend/C4A/src/main/java/eu/city4age/dashboard/api/service/impl/SifarnikServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package eu.city4age.dashboard.api.service.impl;
-
-import eu.city4age.dashboard.api.service.SifarnikService;
-
-public class SifarnikServiceImpl implements SifarnikService {
-
-
-
-}
--- backend/C4A/src/main/java/eu/city4age/dashboard/api/ws/SifarnikService.java
+++ backend/C4A/src/main/java/eu/city4age/dashboard/api/ws/SifarnikService.java
@@ -1,6 +1,8 @@
 package eu.city4age.dashboard.api.ws;
 
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -19,6 +21,8 @@ import com.fasterxml.jackson.datatype.hibernate3.Hibernate3Module;
 
 import eu.city4age.dashboard.api.dao.RiskStatusDAO;
 import eu.city4age.dashboard.api.dao.StakeholderDao;
+import eu.city4age.dashboard.api.domain.DataValidityStatus;
+import eu.city4age.dashboard.api.domain.OrderBy;
 import eu.city4age.dashboard.api.json.GetAllRolesWrapper;
 import eu.city4age.dashboard.api.model.CdRiskStatus;
 import eu.city4age.dashboard.api.model.CdRole;
@@ -46,6 +50,45 @@ public class SifarnikService {
 		List<CdRiskStatus> riskStatus = riskStatusDAO.getAllRiskStatus();
 		
 		String dtoAsString = objectMapper.writeValueAsString(riskStatus);
+        
+        return dtoAsString;
+	}
+	
+	@GET
+    @Path("/getAllDataValitidityStatus")
+    @Produces(MediaType.APPLICATION_JSON)
+	public String getAllDataValitidityStatus() throws Exception {
+		
+		ObjectMapper objectMapper = new ObjectMapper();
+		
+		Map<DataValidityStatus, String> statuses = new EnumMap<>(DataValidityStatus.class);
+
+		statuses.put(DataValidityStatus.QUESTIONABLE_DATA,	"Questionable data");
+		statuses.put(DataValidityStatus.FAULTY_DATA,  		"Faulty data");
+		statuses.put(DataValidityStatus.VALID_DATA,      		"Valid data");
+
+		String dtoAsString = objectMapper.writeValueAsString(statuses);
+        
+        return dtoAsString;
+	}
+	
+	@GET
+    @Path("/getAllOrderBy")
+    @Produces(MediaType.APPLICATION_JSON)
+	public String getAllOrderBy() throws Exception {
+		
+		ObjectMapper objectMapper = new ObjectMapper();
+		
+		Map<OrderBy, String> orders = new EnumMap<>(OrderBy.class);
+
+		orders.put(OrderBy.DATE_ASC,				"Date Asc.");
+		orders.put(OrderBy.DATE_DESC,  				"Date Desc.");
+		orders.put(OrderBy.AUTHOR_NAME_ASC, 		"Author Name Asc.");
+		orders.put(OrderBy.AUTHOR_NAME_DESC,		"Author Name Desc.");
+		orders.put(OrderBy.AUTHOR_ROLE_ASC,  		"Author Name Asc.");
+		orders.put(OrderBy.AUTHOR_ROLE_DESC, 		"Author Name Desc.");
+
+		String dtoAsString = objectMapper.writeValueAsString(orders);
         
         return dtoAsString;
 	}
