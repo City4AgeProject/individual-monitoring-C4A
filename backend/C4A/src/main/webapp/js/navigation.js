625bdd807cb3a3785cc286fb007c7ff9afdb4d72
mimarko <mimarko@gmail.com>
2016-12-16 11:32:25 +0100

    mil view available just for presentation

--- backend/C4A/src/main/webapp/js/navigation.js
+++ backend/C4A/src/main/webapp/js/navigation.js
@@ -28,6 +28,8 @@ define(['ojs/ojcore', 'ojs/ojarraytabledatasource'],
                     {name: 'Detection GEF', id: 'detection_gef',
                         iconClass: 'demo-chart-icon-24  demo-icon-font-24 oj-navigationlist-item-icon'},
                     {name: 'Detection GES', id: 'detection_ges',
+                        iconClass: 'demo-chart-icon-24 demo-icon-font-24 oj-navigationlist-item-icon'},
+                    {name: 'Detection GES mil', id: 'detection_ges_mil',
                         iconClass: 'demo-chart-icon-24 demo-icon-font-24 oj-navigationlist-item-icon'}
                 ];
 
